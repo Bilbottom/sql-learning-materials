bd1d8a43bf56822c4d81f98408ca3bf107c6fff8
Bill <25640332+Bilbottom@users.noreply.github.com>
2024-06-07 16:49:31 +0100

    docs: remove coverage, fix typos, and update solutions

--- docs/challenging-sql-problems/solutions/bronze/temperature-anomaly-detection--sql-server.sql
+++ docs/challenging-sql-problems/solutions/bronze/temperature-anomaly-detection--sql-server.sql
@@ -1,29 +1,16 @@
 ```sql
-with temps as (
+with temperatures as (
     select
         site_id,
         reading_datetime,
         temperature,
-        (
-            avg(temperature) over rows_before_site_reading
-            + avg(temperature) over rows_after_site_reading
-        ) / 2 as average_temperature,
-        (
-            count(temperature) over rows_before_site_reading
-            + count(temperature) over rows_after_site_reading
-        ) as count_of_rows
+        (sum(temperature) over rows_around_site_reading - temperature) / 4 as average_temperature,
+        count(*) over rows_around_site_reading as count_of_rows
     from readings
-    window
-        rows_before_site_reading as (
-            partition by site_id
-            order by reading_datetime
-            rows between 2 preceding and 1 preceding
-        ),
-        rows_after_site_reading as (
-            partition by site_id
-            order by reading_datetime
-            rows between 1 following and 2 following
-        )
+    window rows_around_site_reading as (
+        partition by site_id
+        order by reading_datetime rows between 2 preceding and 2 following
+    )
 )
 
 select
@@ -31,11 +18,11 @@ select
     reading_datetime,
     temperature,
     round(average_temperature, 4) as average_temperature,
-    round(100.0 * (temperature - average_temperature) / average_temperature, 4) as percentage_increase
-from temps
+    round(100.0 * (temperature - average_temperature) / temperature, 4) as percentage_increase
+from temperatures
 where 1=1
-    and count_of_rows = 4
-    and (temperature - average_temperature) / average_temperature > 0.1
+    and count_of_rows = 5
+    and (temperature - average_temperature) / temperature > 0.1
 order by
     site_id,
     reading_datetime
