ca16556cf744578147d1c41d8209309d9a601246
Bill <bill@thewallisfamily.co.uk>
2024-06-24 20:33:38 +0100

    🐛 docs: improve datelist encoding solution

--- docs/challenging-sql-problems/problems/gold/encoding-datelist-ints--sample-output.sql
+++ docs/challenging-sql-problems/problems/gold/encoding-datelist-ints--sample-output.sql
@@ -1,7 +1,7 @@
 ```sql
 solution(user_id, last_update, activity_history) as (
     values
-        (1, '2024-01-09'::date, 496),
-        (2, '2024-01-09'::date,   7)
+        (1, '2024-01-12'::date, 3520),
+        (2, '2024-01-12'::date,   28)
 )
 ```
